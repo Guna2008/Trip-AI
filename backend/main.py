cdb2dcf8c63a8d62809814eb037df52378e6b10d
Guna2008 <guna200120084859894@gmail.com>
2026-09-19 14:38:20 +0530

    configure CORS for frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.database import Base, engine
 from backend.routes.auth import router as auth_router
@@ -7,6 +8,18 @@ from backend.routes.trips import router as trips_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Trip AI API")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(trips_router)
